91f8dabc1935f4e36626fa3c8833ee7b3924f77a
Altria <51522497+Altria11043@users.noreply.github.com>
2019-12-26 13:17:36 +0800

    测试更新

---
+++ Vue_test/src/components/1.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <div v-html="message"></div>
+  </div>
+</template>
+
+<script>
+  new Vue({
+    el: '#app',
+    data: {
+      message: '<h1>Vue测试</h1>'
+    }
+  })
+</script>
+
+<style>
+</style>
